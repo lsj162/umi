6c12836c1a160f5db6bc59af9562d7fc67be6b68
刘圣杰 <liushengjie@liushengjiedeMacBook-Pro.local>
2022-01-26 17:55:36 +0800

    fix: 配置优化

--- src/layouts/Headers/index.tsx
+++ src/layouts/Headers/index.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+
 function Page(props: any) {
+  useEffect(() => {
+    console.log('0-0-0-0', new Date().constructor());
+  }, []);
+
   return (
     <>
       <h1>This is headers</h1>
+      <p>{new Date().constructor()}</p>
+
       <div>{props.children}</div>
     </>
   );
